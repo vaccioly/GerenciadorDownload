2701cdfe85928cda6d82dfe20299d6821afb05b3
ifpb <verribeiro@hotmail.com>
2019-06-06 10:19:01 -0300

    Iniciando a classe Downloads

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #Esse é o programa principal do projeto do gerenciador de downloads
+class Downloads:
+""" Classe cadastrar os computadores para efetuar o Downloads"""
+	__cadastro={}
+	def __init__(self, cadastro):
+0
+		
