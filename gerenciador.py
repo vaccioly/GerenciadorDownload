93c04e5cab7c727b58657a06933dd876c59a9cd3
Vinicius Accioly <vaccioly@gmail.com>
2019-08-07 20:25:20 -0300

    Funcao cadastrar computador, listar, importar e exportar, Ajuste nome da classe com inicial maisculo.

--- gerenciador.py
+++ gerenciador.py
@@ -2,17 +2,20 @@ import computador
 import csv
 
 
-class gerenciador:
+class Gerenciador:
     """A classe gerenciador de downloads administra a velocidade que cada computador terá disponível"""
 
     def __init__(self):
-        pass
+        self.computadores = []
+        self.recursos = []
 
-    def cadastrarComputador(self):
-        pass
+    def cadastrarComputador(self,nome):
+        pc = Computador(nome)
+        self.computadores.append(pc)
 
     def listarComputadores(self):
-        pass
+        for c in self.computadores:
+            print (c)
 
     def importarComputador(self, arquivo):
         with open(arquivo + '.csv', newline='') as csvfile:
@@ -21,14 +24,18 @@ class gerenciador:
                 print(row)
             print('Importado com sucesso!')
 
-    def exportarComputador(self, nome):
+    def exportarComputador(self, nome ='Teste'):
         with open(nome + '.csv', 'w', newline='', encoding='utf-8') as csvfile:
-            cabecalho = ['Identificação', 'IP', 'Recurso']
-            write.writeheader = csv.DictWriter(csvfile, cabecalho=cabecalho)
-            write.writeheader()
-            write.writerow({'Identificação': self.__class__.__id, 'IP': '192.168.0.' + self.__class__.__ip,
-                            'Recurso': self.__recurso})
-            print('Exportado com sucesso!!!! ')
+            cabecalho = ['Identificação', 'IP', 'Descrição']
+            arquivos = csv.DictWriter(csvfile, fieldnames=cabecalho)
+            arquivos.writeheader()
+            #write.writerow({'Identificacao':pc.getId()})
+            #arquivos.writerow({'Identificação': Computador.__class__.__id, 'IP': '192.168.0.' + Computador.__class__.__ip,'Recurso': self.__recurso})
+            #print('Exportado com sucesso!!!! ')
+            for pc in self.computadores:
+                #print(i.getIp(),i.getId())
+                arquivos.writerow({'Identificação': pc.getId(), 'IP': pc.getIp(),'Descrição': pc.getDescricao()})
+            print('Computadores exportado com sucesso!!!!')
 
     def setLink(self, link):
         """Modifica a velocidade do link e retorna uma string dessa atualização"""
@@ -39,5 +46,18 @@ class gerenciador:
         """retorna ao usuário uma string com o valor do link"""
         return f'Velocidade de coneção   {self.__link} Mb/s'
 
-    def cadastrarRecurso(self):
-        pass
+    def cadastrarRecurso(self, novo_recurso, tamanhoNovo):
+        self.recurso = novo_recurso
+        self.tamanho = tamanhoNovo
+        self.recursos.append(self.recurso)
+        self.recursos.append(self.tamanho)
+
+    def listarRecurso(self):
+        return f'Arquivo {self.recurso}, tamanho {self.tamanho} Kbps'
+
+"""adm = Gerenciador()
+adm.cadastrarComputador('ifpb01')
+adm.listarComputadores()
+adm.cadastrarRecurso('documento.txt',1500)
+print(adm.listarRecurso())
+adm.exportarComputador()
